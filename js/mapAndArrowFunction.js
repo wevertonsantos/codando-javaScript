e14d9fedbc375101e4df62dbf1c57ccedcdf6022
Weverton Santos <wever-santos@hotmail.com>
2022-07-26 21:30:17 -0300

    Utilizando Map e ArrowFunction

---
+++ js/mapAndArrowFunction.js
@@ -0,0 +1,8 @@
+// Utilizando arrow function e map.
+// Criar um array de números e manipular os números desse array,
+// retornando um array novo com os números do primeiro array
+// multiplicado por 2
+
+const numeros = [2, 3, 6, 5, 10]
+
+console.log('newArray', numeros.map((numero) => numero * 2))
